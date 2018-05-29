405b256bc795284d899e3f5bc10e88aacc566d28
caike <caikem1.costa@gmail.com>
2018-05-29 09:45:14 -0300

    admin - produtos

--- vendor/hcodebr/php-classes/src/Model/Product.php
+++ vendor/hcodebr/php-classes/src/Model/Product.php
@@ -1,108 +1,121 @@
 <?php 
-
 namespace Hcode\Model;
-
 use \Hcode\DB\Sql;
 use \Hcode\Model;
 use \Hcode\Mailer;
-
 class Product extends Model {
-
 	public static function listAll()
 	{
-
 		$sql = new Sql();
-
 		return $sql->select("SELECT * FROM tb_products ORDER BY desproduct");
+	}
 
-    }
-    
-    public function save()
-    {
+	public static function checkList($list){
+		foreach($list as &$row){
+			$p = new Product();
+			$p->setData($row);
+			$row = $p->getValues();
+		}
+		return $list;
+	}
 
-        $sql = new Sql();
-		
+	public function save()
+	{
+		$sql = new Sql();
 		$results = $sql->select("CALL sp_products_save(:idproduct, :desproduct, :vlprice, :vlwidth, :vlheight, :vllength, :vlweight, :desurl)", array(
-            ":idproduct" =>$this->getidproduct(),
-            ":desproduct" =>$this->getdesproduct(),
-            ":vlprice" =>$this->getvlprice(),
-            ":vlwidth" =>$this->getvlwidth(),
-            ":vlheight" =>$this->getvlheight(),
-            ":vllength" =>$this->getvllength(),
-            ":vlweight" =>$this->getvlweight(),
-            ":desurl" =>$this->getdesurl()
-        ));
+			":idproduct"=>$this->getidproduct(),
+			":desproduct"=>$this->getdesproduct(),
+			":vlprice"=>$this->getvlprice(),
+			":vlwidth"=>$this->getvlwidth(),
+			":vlheight"=>$this->getvlheight(),
+			":vllength"=>$this->getvllength(),
+			":vlweight"=>$this->getvlweight(),
+			":desurl"=>$this->getdesurl()
+		));
+		$this->setData($results[0]);
+	}
+	public function get($idproduct)
+	{
+		$sql = new Sql();
+		$results = $sql->select("SELECT * FROM tb_products WHERE idproduct = :idproduct", [
+			':idproduct'=>$idproduct
+		]);
+		$this->setData($results[0]);
+	}
+	public function delete()
+	{
+		$sql = new Sql();
+		$sql->query("DELETE FROM tb_products WHERE idproduct = :idproduct", [
+			':idproduct'=>$this->getidproduct()
+		]);
+	}
+	public function checkPhoto()
+	{
+		if (file_exists(
+            $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 
+            "ecommercee" . DIRECTORY_SEPARATOR . 
+			"res" . DIRECTORY_SEPARATOR . 
+			"site" . DIRECTORY_SEPARATOR . 
+			"img" . DIRECTORY_SEPARATOR . 
+			"products" . DIRECTORY_SEPARATOR . 
+			$this->getidproduct() . ".jpg"
+			)) {
+			$url = "/ecommercee/res/site/img/products/" . $this->getidproduct() . ".jpg";
+		} else {
+			$url = "/ecommercee/res/site/img/product.jpg";
+		}
+		return $this->setdesphoto($url);
+	}
+	public function getValues()
+	{
+		$this->checkPhoto();
+		$values = parent::getValues();
+		return $values;
+	}
+	public function setPhoto($file)
+	{
+		$extension = explode('.', $file['name']);
+		$extension = end($extension);
+		switch ($extension) {
+			case "jpg":
+			case "jpeg":
+			$image = imagecreatefromjpeg($file["tmp_name"]);
+			break;
+			case "gif":
+			$image = imagecreatefromgif($file["tmp_name"]);
+			break;
+			case "png":
+			$image = imagecreatefrompng($file["tmp_name"]);
+			break;
+		}
+        $dist = $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 
+            "ecommercee" . DIRECTORY_SEPARATOR . 
+			"res" . DIRECTORY_SEPARATOR . 
+			"site" . DIRECTORY_SEPARATOR . 
+			"img" . DIRECTORY_SEPARATOR . 
+			"products" . DIRECTORY_SEPARATOR . 
+			$this->getidproduct() . ".jpg";
+		imagejpeg($image, $dist);
+		imagedestroy($image);
+		$this->checkPhoto();
+	}
 
-        $this->setData($results[0]);
-
-             
-    }
-
-    public function get($idproduct)
-    {
-
-        $sql = new Sql();
-
-        $results = $sql->select("SELECT * FROM tb_products WHERE idproduct = :idproduct", [
-            ':idproduct'=>$idproduct
-
-        ]);
-
-        $this->setData($results[0]);
-        
-    }
-
-    public function delete()
-    {
-
-        $sql = new Sql();
-
-        $sql->query("DELETE FROM tb_products WHERE idproduct = :idproduct", [
-            ':idproduct'=>$this->getidproduct()
-        ]);
-
-    }
-
-    public function checkPhoto()
-    {
-
-        if (file_exists(
-        $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 
-        "ecommercee" . DIRECTORY_SEPARATOR . 
-        "res" . DIRECTORY_SEPARATOR . 
-        "site" . DIRECTORY_SEPARATOR . 
-        "img" . DIRECTORY_SEPARATOR . 
-        "products" . DIRECTORY_SEPARATOR . 
-        $this->getidproduct() . ".jpg"
-        )) {
-
-            $url = "/ecommercee/res/site/img/products/" . $this->getidproduct() . ".jpg";
-
-        } else {
-
-            $url = "/ecommercee/res/site/img/product.jpg";
-
-        }
-
-        return $this->setdesphoto($url);
-
-    }
-
-    public function getValues()
-    {
-        $this->checkPhoto();
-
-        $values = parent::getValues();
-
-        return $values;
-
-
-    }
-
-    
-    
-
+	public function getFromURL($desurl)
+	{
+		$sql = new Sql();
+		$rows = $sql->select("SELECT * FROM tb_products WHERE desurl = :desurl LIMIT 1", [
+			':desurl'=>$desurl
+		]);
+		$this->setData($rows[0]);
+	}
+	public function getCategories()
+	{
+		$sql = new Sql();
+		return $sql->select("
+			SELECT * FROM tb_categories a INNER JOIN tb_productscategories b ON a.idcategory = b.idcategory WHERE b.idproduct = :idproduct
+		", [
+			':idproduct'=>$this->getidproduct()
+		]);
+	}
 }
-
-
-?>
+ ?>
--- views-cache/categories-menu.18dc39f5ef94c5c57d33a29a6c1731e8.rtpl.php
+++ views-cache/categories-menu.18dc39f5ef94c5c57d33a29a6c1731e8.rtpl.php
@@ -1,1 +1,1 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/ecommercee/categories/1">Caike </a></li><li><a href="/ecommercee/categories/17">Caike KING</a></li><li><a href="/ecommercee/categories/10">Caike Mercadoria</a></li><li><a href="/ecommercee/categories/18">Caike NBA</a></li><li><a href="/ecommercee/categories/15">Caike Rap</a></li><li><a href="/ecommercee/categories/16">Caike TI</a></li>
+<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/ecommercee/categories/2">Caike</a></li><li><a href="/ecommercee/categories/3">Caike 123</a></li>
